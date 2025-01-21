3d0ae54eb765fe725454987ff1aa4e71547b8427
NataliaBarmina <natbar73@mail.ru>
2025-01-21 16:26:41 +0300

    added screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "320px",
+      s: "540px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     colors: {
       black: colors.black,
       white: colors.white,
