2dc9f8cbbae4a637773e92566b058726fa88d21a
NataliaBarmina <natbar73@mail.ru>
2025-02-27 15:20:54 +0300

    create staticFieldForQuestionsAndAnswers

--- src/components/creating/creatingQuestionsByAI/questionsCreatedByAI.tsx
+++ src/components/creating/creatingQuestionsByAI/questionsCreatedByAI.tsx
@@ -1,28 +1,8 @@
 import { Question } from "../../../common/dataExample";
-// import ListOfQuestions from "../../../components/commonComponents/listOfQuestions";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
 import classNames from "classnames";
 import Alert from "../../commonComponents/alertDialog";
 import FormForEditingQuestions from "../../commonComponents/formForEditingQuestins";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "../../../chadcnComponents/ui/form";
-import { Textarea } from "../../../chadcnComponents/ui/textarea";
-// import { Topic } from "../../common/dataExample";
-
-const schema = yup.object({
-  questionExample: yup.string(),
-  answerExample1: yup.string(),
-  answerExample2: yup.string(),
-  answerExample3: yup.string(),
-});
+import BlokedFieldWithAnswersAndQuestions from "../../../components/commonComponents/blokedFieldWithAnswersAndQuestions";
 
 //todo: типизация для пропсов, которые будут предаваться в QuestionsCreatedByAI
 type QuestionsCreatedByAIProps = {
@@ -52,13 +32,6 @@ const arr = [
 const QuestionsCreatedByAI = () => {
   //TODO: должно приходить в пропсах название курса и массив с объектами вопросов
   ///TODO: передавть в пропсах массив с объектами вопросов
-  const form = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  function onSubmit(values: any) {
-    console.log(values);
-  }
   return (
     <div className="pb-1">
       <div className="p-4 text-[150%] font-bold">
@@ -78,121 +51,44 @@ const QuestionsCreatedByAI = () => {
             "2xl:w-[45vw]",
           )}
         >
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <div className="py-6 text-lg font-bold text-blue-100">{`вопрос №${index + 1}`}</div>
-              <FormField
-                control={form.control}
-                name="questionExample"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Textarea
-                        className="placeholder:text-black disabled:bg-pink-100 disabled:text-lg"
-                        placeholder={item.вопрос}
-                        disabled={true}
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
+          <div className="py-6 text-lg font-bold text-blue-100">{`вопрос №${index + 1}`}</div>
+
+          <BlokedFieldWithAnswersAndQuestions
+            question={item.вопрос}
+            correctAnswer={item.ответ_1}
+            wrongAnswer1={item.ответ_2}
+            wrongAnswer2={item.ответ_3}
+          />
+
+          <div className="mb-8 flex w-full justify-evenly py-8">
+            <div>
+              <Alert
+                whatToDo={"сохраняем данные в стэйт"}
+                alertDialogTitle={"Вы уверены?"}
+                alertDialogDescription={
+                  "Внимательно проверьте полученные вопросы и ответы. Правильный ответ должен находиться на первом месте"
+                }
+                alertDialogAction={"продолжить редактирование"}
+                alertDialogCancel={"сохранить вопрос"}
+                buttonName={"сохранить"}
               />
-              <div className="mx-auto w-[85%]">
-                <FormField
-                  control={form.control}
-                  name="answerExample1"
-                  render={({ field }) => (
-                    <FormItem className="mt-4">
-                      <FormLabel className="text-yellow-50">
-                        ВВЕДИТЕ ПРАВИЛЬНЫЙ ОТВЕТ
-                      </FormLabel>
-                      <FormControl>
-                        <Textarea
-                          className="disabled:text-md placeholder:text-black disabled:bg-pink-100"
-                          placeholder={item.ответ_1}
-                          disabled={true}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="answerExample2"
-                  render={({ field }) => (
-                    <FormItem className="mt-4">
-                      <FormLabel className="text-yellow-50">
-                        {" "}
-                        ВВЕДИТЕ НЕПРАВИЛЬНЫЙ ОТВЕТ
-                      </FormLabel>
-                      <FormControl>
-                        <Textarea
-                          className="disabled:text-md placeholder:text-black disabled:bg-pink-100"
-                          placeholder={item.ответ_2}
-                          disabled={true}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="answerExample3"
-                  render={({ field }) => (
-                    <FormItem className="mt-4">
-                      <FormLabel className="text-yellow-50">
-                        {" "}
-                        ВВЕДИТЕ НЕПРАВИЛЬНЫЙ ОТВЕТ
-                      </FormLabel>
-                      <FormControl>
-                        <Textarea
-                          className="disabled:text-md placeholder:text-black disabled:bg-pink-100"
-                          placeholder={item.ответ_3}
-                          disabled={true}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="mb-8 flex w-full justify-evenly py-8">
-                <div>
-                  <Alert
-                    whatToDo={"сохраняем данные в стэйт"}
-                    alertDialogTitle={"Вы уверены?"}
-                    alertDialogDescription={
-                      "Внимательно проверьте полученные вопросы и ответы. Правильный ответ должен находиться на первом месте"
-                    }
-                    alertDialogAction={"продолжить редактирование"}
-                    alertDialogCancel={"сохранить вопрос"}
-                    buttonName={"сохранить"}
-                  />
-                </div>
-                <div>
-                  <FormForEditingQuestions />
-                </div>
-                <div>
-                  <Alert
-                    whatToDo={"удаляем данные из стэйта"}
-                    alertDialogTitle={"Вы уверены?"}
-                    alertDialogDescription={
-                      "Это действие нельзя отменить. Вопрос будет полностью удален."
-                    }
-                    alertDialogAction={"продолжить редактирование"}
-                    alertDialogCancel={"удалить вопрос"}
-                    buttonName={"удалить"}
-                  />
-                </div>
-              </div>
-            </form>
-          </Form>
+            </div>
+            <div>
+              <FormForEditingQuestions />
+            </div>
+            <div>
+              <Alert
+                whatToDo={"удаляем данные из стэйта"}
+                alertDialogTitle={"Вы уверены?"}
+                alertDialogDescription={
+                  "Это действие нельзя отменить. Вопрос будет полностью удален."
+                }
+                alertDialogAction={"продолжить редактирование"}
+                alertDialogCancel={"удалить вопрос"}
+                buttonName={"удалить"}
+              />
+            </div>
+          </div>
         </div>
       ))}
     </div>
--- src/components/questionnaire/resultsOfTheQuestionnaire.tsx
+++ src/components/questionnaire/resultsOfTheQuestionnaire.tsx
@@ -1,37 +1,13 @@
-import { useForm, SubmitHandler } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Textarea, Input } from "../../common/createFields";
-import { TFields } from "../../common/createFields";
 import classNames from "classnames";
 import { questionSet } from "../../common/dataExample";
-
-const schema = yup.object({
-  questionForAnalysis: yup.string(),
-  rightAnswerForAnalysis: yup.string(),
-  wrongAnswerForAnalysis: yup.string(),
-  stringRightAnswerForAnalysis: yup.string(),
-  stringWrongAnswerForAnalysis: yup.string(),
-});
+import { Label } from "../../chadcnComponents/ui/label";
+import StaticFieldForQuestionsAndAnswers from "../commonComponents/staticFieldForQuestionsAndAnswers";
 
 const ResultsOfTheQuestionnaire = () => {
   const percentageOfCorrectAnswers = "95%";
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({
-    mode: "onChange",
-    resolver: yupResolver(schema),
-  });
-
-  const onSubmit: SubmitHandler<TFields> = (data) => {
-    console.log(data);
-  };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
       <div className="p-8 text-[150%] font-bold">
         Поздравляем! <br /> Вы ответили на вопросы анкеты.
       </div>
@@ -48,66 +24,41 @@ const ResultsOfTheQuestionnaire = () => {
           <div
             className={classNames(
               "mx-auto mb-8 w-[95%] bg-green-800",
-              "rounded-2xl border-2 border-solid border-gray-600",
+              "rounded-2xl",
+              "border-2 border-solid border-gray-600",
               "md:w-[85%]",
             )}
           >
             <div className="mb-2 p-4 text-lg font-bold text-blue-100">{`вопрос №${index + 1}`}</div>
-            <div className={classNames("mx-auto mb-8 w-[100%]", "s:w-[90%]")}>
-              <div className="mb-3">
-                <Textarea
-                  placeholder={`${Object.keys(item)}`}
-                  register={register}
-                  fieldName="questionForAnalysis"
-                  defaultValue=""
-                  disabled={true}
-                  styles=""
+            <div className="mx-auto w-[90%]">
+              <StaticFieldForQuestionsAndAnswers
+                styles="mb-6"
+                value={`${Object.keys(item)}`}
+                id={""}
+              />
+              <div className="mx-auto w-[90%]">
+                <Label className="text-white" htmlFor="correctAnswer">
+                  ПРАВИЛЬНЫЙ ОТВЕТ
+                </Label>
+                <StaticFieldForQuestionsAndAnswers
+                  styles="mb-6"
+                  value={`${Object.values(item)[0][1]}`}
+                  id={"correctAnswer"}
                 />
-              </div>
-              <div className="mx-auto mb-6 flex justify-center">
-                <div className="mr-[-3px] w-[44%]">
-                  <Textarea
-                    placeholder=""
-                    register={register}
-                    fieldName="stringRightAnswerForAnalysis"
-                    defaultValue="правильный ответ"
-                    disabled={true}
-                    styles="px-1"
-                  />
-                  <Textarea
-                    placeholder=""
-                    register={register}
-                    fieldName="stringWrongAnswerForAnalysis"
-                    defaultValue="ваш ответ"
-                    disabled={true}
-                    styles=""
-                  />
-                </div>
-                <div className="w-[56%]">
-                  <Textarea
-                    placeholder={`${Object.values(item)[0][1]}`}
-                    register={register}
-                    fieldName="rightAnswerForAnalysis"
-                    defaultValue=""
-                    disabled={true}
-                    styles=""
-                  />
-
-                  <Textarea
-                    placeholder={`${Object.values(item)[0][2]}`}
-                    register={register}
-                    fieldName="wrongAnswerForAnalysis"
-                    defaultValue=""
-                    disabled={true}
-                    styles=""
-                  />
-                </div>
+                <Label className="text-white" htmlFor="yourAnswer">
+                  ВАШ ОТВЕТ
+                </Label>
+                <StaticFieldForQuestionsAndAnswers
+                  styles="mb-8"
+                  value={`${Object.values(item)[0][2]}`}
+                  id={"yourAnswer"}
+                />
               </div>
             </div>
           </div>
         ))}
       </div>
-    </form>
+    </div>
   );
 };
 export default ResultsOfTheQuestionnaire;
