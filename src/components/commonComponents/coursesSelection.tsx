47975b1d6e7f81ac1ccaada4833e448475d3de89
NataliaBarmina <natbar73@mail.ru>
2025-03-25 17:34:46 +0300

    fix coursesSelection.tsx

--- src/components/commonComponents/coursesSelection.tsx
+++ src/components/commonComponents/coursesSelection.tsx
@@ -3,13 +3,14 @@ import { useState } from "react";
 import { theme } from "@common/themeForMaterialUI";
 import { useLocation } from "react-router-dom";
 import ThemesSelection from "./themesSelection";
-import { arr, TSubject } from "@common/dataExample"; //todo: получаем доступ к стэйту - массиву с вопросами
+import { arr, TSubject, TTopic } from "@common/dataExample"; //todo: получаем доступ к стэйту - массиву с вопросами
 
 type TabPanelProps = {
   children?: React.ReactNode;
   index: number;
   value: number;
 };
+type HandleTabChange = (event: React.SyntheticEvent, newValue: number) => void;
 
 const tabStyle = {
   backgroundColor: "#767474",
@@ -51,75 +52,103 @@ function CustomTabPanel(props: TabPanelProps) {
 const CoursesSelection = () => {
   const [tabValue, setTabValue] = useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange: HandleTabChange = (event, newValue) => {
     setTabValue(newValue);
   };
 
-  const location = useLocation();
-  const buttonLabel = location.state?.buttonLabel.toLowerCase();
+  const courseNames: string[] = arr.map(
+    (item: TSubject) => Object.keys(item)[0],
+  );
+  const selectedCourseName = courseNames[tabValue];
+  const courseEntry = arr.find(
+    (item) => Object.keys(item)[0] === selectedCourseName,
+  );
+  const courseThemes = courseEntry[selectedCourseName] ?? [];
 
   return (
     <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          width: "100%",
-          paddingBottom: "45px",
-        }}
-      >
+      <div className="w-full pb-11">
         <div className="bg-stone-300 shadow-2xl shadow-stone-500">
           <div className="py-10 text-xl font-bold">Выберите курс</div>
-          <Box
-            sx={{
-              maxWidth: { xs: "98vw", md: "68vw", lg: "55vw" },
-              display: "inline-block",
-              margin: " auto",
-            }}
-          >
-            <Tabs
-              value={tabValue}
-              onChange={handleChange}
-              textColor="secondary"
-              variant="scrollable"
-              scrollButtons="auto"
-              aria-label="scrollable auto tabs example"
-              sx={{
-                "& .MuiTabs-indicator": {
-                  backgroundColor: "black",
-                },
-              }}
-            >
-              {arr.map((item: TSubject, index: number) => (
-                <Tab
-                  sx={tabStyle}
-                  label={Object.keys(item)[0]} // перебор названий всех курсов
-                  key={index}
-                  id={`simple-tab-${index}`}
-                  aria-controls={`simple-tabpanel-${index}`}
-                />
-              ))}
-            </Tabs>
-          </Box>
-        </div>
 
-        {arr.map((item: TSubject, index: number) => (
-          <CustomTabPanel value={tabValue} index={index} key={index}>
-            <ThemesSelection
-              course={Object.keys(item)[0]} // название темы
-              listOfThemes={Object.values(item)[0]} // массив вопросов с темами
-              buttonLabel={buttonLabel}
-            />
-          </CustomTabPanel>
-        ))}
-      </Box>
+          <TabsContainer
+            tabValue={tabValue}
+            courseNames={courseNames}
+            handleChange={handleChange}
+          />
+        </div>
+        <CustomTabPanelContainer
+          tabValue={tabValue}
+          selectedCourseName={selectedCourseName}
+          courseThemes={courseThemes}
+        />
+      </div>
     </ThemeProvider>
   );
 };
 export default CoursesSelection;
 
-//! образец как можно менять свойства, если sx не разрешен
-// import { styled } from "@mui/system";
-// const CustomStyledTabPanel = styled(CustomTabPanel)(({ theme }) => ({
-//   backgroundColor: "red", // Пример для изменения фона
-//   padding: "20px", // Пример для изменения отступов
-//   borderRadius: "8px", // Пример для изменения углов
-// }));
+type MyProps = {
+  tabValue?: number;
+  selectedCourseName?: string;
+  courseThemes?: TTopic[];
+  courseNames?: string[];
+  handleChange?: HandleTabChange;
+};
+
+const TabsContainer = ({ tabValue, courseNames, handleChange }: MyProps) => {
+  return (
+    <Box
+      sx={{
+        maxWidth: { xs: "98vw", md: "68vw", lg: "55vw", xl: "100%" },
+        display: "inline-block",
+        margin: " auto",
+      }}
+    >
+      <Tabs
+        value={tabValue}
+        onChange={handleChange}
+        textColor="secondary"
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+        sx={{
+          "& .MuiTabs-indicator": {
+            backgroundColor: "black",
+          },
+        }}
+      >
+        {courseNames.map((item: string, index: number) => (
+          <Tab
+            sx={tabStyle}
+            label={item} // перебор названий всех курсов
+            key={index}
+            id={`simple-tab-${index}`}
+            aria-controls={`simple-tabpanel-${index}`}
+          />
+        ))}
+      </Tabs>
+    </Box>
+  );
+};
+
+const CustomTabPanelContainer = ({
+  tabValue,
+  selectedCourseName,
+  courseThemes,
+}: MyProps) => {
+  const location = useLocation();
+  const buttonLabel = location.state?.buttonLabel.toLowerCase();
+
+  return (
+    <div>
+      <CustomTabPanel value={tabValue} index={tabValue}>
+        <ThemesSelection
+          course={selectedCourseName} // название темы
+          listOfThemes={courseThemes} // массив вопросов с темами
+          buttonLabel={buttonLabel}
+        />
+      </CustomTabPanel>
+    </div>
+  );
+};
