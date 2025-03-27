8f845bf41973e27c1f71f8d46660a192049b0349
NataliaBarmina <natbar73@mail.ru>
2025-03-27 11:27:49 +0300

    refactoring Creating

--- src/components/creating/creating.tsx
+++ src/components/creating/creating.tsx
@@ -1,44 +1,42 @@
 import { useNavigate } from "react-router-dom";
-import classNames from "classnames";
 import Button from "@commonComponents/buttons";
 import { useTranslation } from "react-i18next";
+import { cn } from "@lib/utils";
 
-const Creating = () => {
+const circleStyles = cn(
+  "mx-auto rounded-full bg-[rgb(48,80,80)]/85",
+  "h-[80vw] w-[80vw]",
+  "s:h-[65vw] s:w-[65vw]",
+  "md:landscape:h-[60vw] md:landscape:w-[60vw]",
+  "lg:landscape:h-[48vw] lg:landscape:w-[48vw]",
+  "xl:landscape:h-[40vw] xl:landscape:w-[40vw]",
+  "2xl:landscape:h-[32vw] 2xl:landscape:w-[32vw]",
+);
+
+const Creating: React.FC = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
+
+  const handleNavigate = (labelKey: string) => {
+    navigate("/coursesSelection", {
+      state: { buttonLabel: t(labelKey) },
+    });
+  };
 
   return (
     <div className="pb-6">
       <div className="pb-8 pt-12 text-[150%] font-bold">{t("link.create")}</div>
-      <div
-        className={classNames(
-          "mx-auto rounded-full bg-[rgb(48,80,80)]/85",
-          "h-[80vw] w-[80vw]",
-          "s:h-[65vw] s:w-[65vw]",
-          "md:landscape:h-[60vw] md:landscape:w-[60vw]",
-          "lg:landscape:h-[48vw] lg:landscape:w-[48vw]",
-          "xl:landscape:h-[40vw] xl:landscape:w-[40vw]",
-          "xl:landscape:h-[32vw] xl:landscape:w-[32vw]",
-        )}
-      >
+      <div className={circleStyles}>
         <div className="flex h-[100%] w-[100%] flex-col justify-evenly">
           <Button
             buttonLabel={t("buttonLabel.byAI")}
             size="big"
-            onClick={() =>
-              navigate("/coursesSelection", {
-                state: { buttonLabel: t("buttonLabel.byAI") },
-              })
-            }
+            onClick={() => handleNavigate("buttonLabel.byAI")}
           />
           <Button
             buttonLabel={t("buttonLabel.yourself")}
             size="big"
-            onClick={() =>
-              navigate("/coursesSelection", {
-                state: { buttonLabel: t("buttonLabel.yourself") },
-              })
-            }
+            onClick={() => handleNavigate("buttonLabel.yourself")}
           />
         </div>
       </div>
