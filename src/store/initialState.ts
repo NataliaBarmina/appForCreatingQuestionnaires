44a998621577177f5a645c255656a68511ecc1ce
NataliaBarmina <natbar73@mail.ru>
2025-05-20 11:10:31 +0300

    put initialState into separate file

---
+++ src/store/initialState.ts
@@ -0,0 +1,14 @@
+import { TQuizState } from "./commonTypes";
+
+export const initialState: TQuizState = {
+  quizData: [
+    { JavaScript: [] },
+    { CSS: [] },
+    { TypeScript: [] },
+    { HTML: [] },
+    { Git: [] },
+    { React: [] },
+    { Cmd: [] },
+    { Прочее: [] },
+  ],
+};
