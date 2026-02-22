b62ee20f09c47ce3ab689e1537403dea55740ca6
NataliaBarmina <natbar73@mail.ru>
2026-02-22 17:13:32 +0300

    Add AuthForm with Firebase login/logout

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -5,7 +5,7 @@ export const classesForLinks = cn(
   "text-center text-[140%] text-white",
   "shadow-lg shadow-stone-900",
   "hover:text-pink-900 focus:text-emerald-400",
-  "md:mb-[8vh] md:shadow-none lg:mb-[11vh]",
+  "md:mb-[8vh] md:shadow-none lg:mb-[11vh]"
 );
 
 export const classesForContainer = cn(
@@ -13,7 +13,7 @@ export const classesForContainer = cn(
   "md:min-h-dvh md:bg-black",
   "lg:w-[85%]",
   "xl:w-[80%]",
-  "2xl:w-[70%]",
+  "2xl:w-[70%]"
 );
 export const classesForHead = cn(
   "z-50 mb-2 h-[12vh] w-[100%]",
@@ -21,22 +21,22 @@ export const classesForHead = cn(
   "md:fixed md:left-0 md:top-0",
   "lg:left-[7.5vw] lg:w-[85%]",
   "xl:left-[10vw] xl:w-[80%]",
-  "2xl:left-[14.9vw] 2xl:w-[70%]",
+  "2xl:left-[14.9vw] 2xl:w-[70%]"
 );
 export const classesForNavPanel = cn(
   "mx-auto block w-screen align-top",
   "shadow-lg shadow-[#241515]",
   "md:inline-block md:min-h-[86vh] md:w-[30%]",
-  "lg:pt-[13vh]",
+  "lg:pt-[13vh]"
 );
 export const classesForFixingNavPanel = cn(
-  "md:fixed md:left-0 md:top-[30vh] md:w-[30%]",
+  "md:fixed md:left-0 md:top-[25vh] md:w-[30%]",
   "lg:left-[7.5vw] lg:w-[25.5vw]",
   "xl:left-[10vw] xl:w-[24vw]",
-  "2xl:left-[15vw] 2xl:w-[20.5vw]",
+  "2xl:left-[15vw] 2xl:w-[20.5vw]"
 );
 export const classesForOutlet = cn(
   "mx-auto w-screen bg-[#D3D6D6] text-center",
   "shadow-lg shadow-[#241515]",
-  "md:inline-block md:min-h-[86vh] md:w-[70%]",
+  "md:inline-block md:min-h-[86vh] md:w-[70%]"
 );
