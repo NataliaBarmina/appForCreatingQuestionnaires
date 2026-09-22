f3f7a21bbbc0d6c06e1cac9e780df7b9b3a7166a
NataliaBarmina <natbar73@mail.ru>
2026-09-22 17:28:01 +0300

    improve course-selection layout

--- src/pages/course-selection-page.tsx
+++ src/pages/course-selection-page.tsx
@@ -6,9 +6,12 @@ export const CourseSelectionPage = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
-      <h1>{t("courseThemeSelection.courseSelection")}</h1>
-      <CourseWheel />
+    <div className="flex h-[calc(100dvh-9rem)] min-h-0 w-full min-w-0 flex-col md:h-[calc(100dvh-11rem)] lg:h-[88vh]">
+      <h1 className="shrink-0">{t("courseThemeSelection.courseSelection")}</h1>
+
+      <div className="flex min-h-0 min-w-0 flex-1 items-center justify-center overflow-hidden p-4 md:p-8">
+        <CourseWheel />
+      </div>
     </div>
   );
 };
